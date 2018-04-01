303001bc89c5e3b85a9d8b69100c3c5ed6bfa583
daniel <d.schulz93@gmx.de>
2018-04-01 20:06:01 +0200

    Load data

--- src/app/Main.js
+++ src/app/Main.js
@@ -1,77 +1,44 @@
-/**
- * In this file, we create a React component
- * which incorporates components provided by Material-UI.
- */
 import React, {Component} from 'react';
-import RaisedButton from 'material-ui/RaisedButton';
-import Dialog from 'material-ui/Dialog';
 import {deepOrange500} from 'material-ui/styles/colors';
-import FlatButton from 'material-ui/FlatButton';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import {Tabs, Tab} from 'material-ui/Tabs';
+import {ApiClient, DefaultApi} from '../../client/src/index';
 
-const styles = {
-    container: {
-        textAlign: 'center',
-        paddingTop: 200,
-    },
+let client = new ApiClient();
+client.basePath = 'http://localhost:8888';
+
+let api = new DefaultApi(client);
+
+let callback = function(error, data, response) {
+    if (error) {
+        alert(error);
+    } else {
+        alert(JSON.stringify(response.body));
+    }
 };
-
 const muiTheme = getMuiTheme({
     palette: {
         accent1Color: deepOrange500,
     },
 });
 
+function handleActive(tab) {
+    api.techniqueGet(callback);
+}
+
 class Main extends Component {
     constructor(props, context) {
         super(props, context);
-
-        this.state = {
-            open: false,
-        };
-    }
-
-    handleRequestClose = () => {
-        this.setState({
-            open: false,
-        });
-    }
-
-    handleTouchTap = () => {
-        this.setState({
-            open: true,
-        });
     }
 
     render() {
-        const standardActions = (
-            <FlatButton
-                label="Ok"
-                primary={true}
-                onTouchTap={this.handleRequestClose}
-            />
-        );
-
         return (
             <MuiThemeProvider muiTheme={muiTheme}>
-                <div style={styles.container}>
-                    <Dialog
-                        open={this.state.open}
-                        title="Super Secret Password"
-                        actions={standardActions}
-                        onRequestClose={this.handleRequestClose}
-                    >
-                        1-2-3-4-5
-                    </Dialog>
-                    <h1>Material-UI</h1>
-                    <h2>example project</h2>
-                    <RaisedButton
-                        label="Super Secret Password"
-                        secondary={true}
-                        onTouchTap={this.handleTouchTap}
-                    />
-                </div>
+                <Tabs>
+                    <Tab label="Techniques" onActive={handleActive}>
+                    </Tab>
+                </Tabs>
             </MuiThemeProvider>
         );
     }
--- src/app/app.js
+++ src/app/app.js
@@ -2,27 +2,6 @@ import React from 'react';
 import {render} from 'react-dom';
 import injectTapEventPlugin from 'react-tap-event-plugin';
 import Main from './Main'; // Our custom react component
-import {ApiClient, DefaultApi, Technique} from '../../client/src/index';
-import ExerciseData from "../../client/src/model/ExerciseData";
-
-let client = new ApiClient();
-client.basePath = 'http://localhost:8888';
-
-let api = new DefaultApi(client);
-
-let callback = function(error, data, response) {
-    if (error) {
-        console.error(error);
-    } else {
-        console.log(response);
-    }
-};
-
-let technique = new Technique();
-technique.data = new ExerciseData();
-technique.data.description = "de";
-
-api.techniquePost(technique,callback);
 
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
