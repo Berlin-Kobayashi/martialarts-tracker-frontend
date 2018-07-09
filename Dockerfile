78ebc5696884f0a54879ca099defe69ba4512be5
Daniel Schulz <daniel.schulz@ocono.de>
2018-07-09 12:01:21 +0200

    Add basic auth

--- Dockerfile
+++ Dockerfile
@@ -10,4 +10,6 @@ RUN npm run-script start
 
 FROM nginx:1.15.1
 
+COPY ops/nginx /
+
 COPY --from=build /usr/src/app/build /usr/share/nginx/html/
--- ops/build.sh
+++ ops/build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker build --build-arg API=http://jsonmancer.com:81 -t danshu93/ma-tracker:latest `dirname "$0"`/..
+docker build --build-arg API=http://ma-tracker.jsonmancer.com:81 -t danshu93/ma-tracker:latest `dirname "$0"`/..
