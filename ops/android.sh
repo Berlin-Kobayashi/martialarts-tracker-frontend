f7de1e6d8a52aa7f28fc1e3072f18814fe2ce422
Daniel Schulz <daniel.schulz@ocono.de>
2018-07-15 20:57:22 +0200

    Configure android app

--- ops/android.sh
+++ ops/android.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
-docker build -t cordova -f Dockerfile.cordova `dirname "$0"`/..
+docker build --build-arg API=http://ma-tracker.jsonmancer.com:81 --build-arg TOKEN=$TOKEN -t cordova -f Dockerfile.cordova `dirname "$0"`/..
 
 docker run --rm -v `pwd`/apps:/usr/src/cordova/platforms/android/build/outputs/apk cordova
